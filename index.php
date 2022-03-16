7f1a6da64d76a6a2e7799a85ea7147756e5c0303
Micka73100 <Michael-73@live.Fr>
2022-03-16 08:57:24 +0100

    Projet maquette

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./styleblock.css">
+    <title>LE FLEURISTE</title>
+</head>
+<body>
+<div class="container">
+    <div class="item1"><img class="logo1" src="./img/logo1.png"/></img></div>
+    <div class="item2"><img class="background" alt="" src="./img/background.jpg"/></img></div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+    <div class="item">4</div>
+
+</div>
+</body>
+</html>
