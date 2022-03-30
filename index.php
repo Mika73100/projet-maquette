d8a2546706b1ad7df2f1db2b4738af4ba3fd48d9
Micka73100 <Michael-73@live.Fr>
2022-03-30 12:23:46 +0200

    maquette

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,14 +9,47 @@
     <link rel="stylesheet" href="./styleblock.css">
     <title>LE FLEURISTE</title>
 </head>
-<body>
-<div class="container">
-    <div class="item1"><img class="logo1" href="index.php" src="./img/logo1.png"/></img></div>
-    <div class="item2"><img class="background" alt="" src="./img/background.png"/></img></div>
-    <div class="item2"><button><a href="contact.php">Nous contacter</a></button></div>
-    <div class="item">3</div>
-    <div class="item">4</div>
 
-</div>
+<body>
+    <div class="container">
+        <div class="item1"><img class="logo1" href="index.php" src="./img/logo1.png" /></img></div>
+
+        <div class="item"><img class="background" alt="" src="./img/background.png" /></img></div>
+
+        <div class="item2"><button class="button"><a href="contact.php">Nous contacter</a></button></div>
+
+
+        <div id="Global">
+        <div id="gauche"><img class="carte" alt="carte" src="./img/carte.jpg" height="auto" width="auto" /></div>
+
+        <div id="droite">
+                    <li>lundi Fermé</li><br>
+                    <li>mardi 09:15–12:15, 15:30–19:00</li><br>
+                    <li>mercredi 09:15–12:15, 15:30–19:00</li><br>
+                    <li>jeudi 09:15–12:15, 15:30–19:00</li><br>
+                    <li>vendredi 09:15–12:15, 15:30–19:00</li><br>
+                    <li>samedi 09:15–12:15, 15:30–19:00</li><br>
+                    <li>dimanche 10:00–12:30</li>
+        </div>
+        </div>
+
+        <div class="item3">4</div>
+
+
+
+
+
+
+
+
+
+
+<footer>
+	<div class="item3">
+        <p>
+            
+        </p>
+	</div>
+</footer>
 </body>
 </html>
